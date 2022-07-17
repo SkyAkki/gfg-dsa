9a34440e203798437a6947bf8110bc7456bce525
Akash Saxena <akashsaxena5555@gmail.com>
2022-07-17 11:54:45 +0530

    add comments to optimize iterativePower

--- Mathematics/iterativePower.js
+++ Mathematics/iterativePower.js
@@ -6,13 +6,13 @@ function iterativePower(num,power){
     let result = 1;
     let original = num;
     while(power > 0){
-        if(power % 2 != 0){ //Bit is 1
+        if(power % 2 != 0){ //Bit is 1;;; otherwise written as power&1
             result = result * original;
         }
         else{
             result = result
         }
-        power = Math.floor(power/2);
+        power = Math.floor(power/2); //otherwise written as n = n>>1
         original = original * original
     }
     console.log(result)
